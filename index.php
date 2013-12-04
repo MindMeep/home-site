42775a90215178a4e2ee50a1b5f7939b3db7d711
Lorenz Bischof <lorenzbischof@gmail.com>
2013-12-04 14:21:53 +0100

    added basic html

--- index.php
+++ index.php
@@ -13,7 +13,35 @@
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
-        <p>Hello world! This is HTML5 Boilerplate.</p>
+        <header>
+        	<h1>MindMeep</h1>
+        	<nav>
+        		<a href="#">Our Work</a>
+        		<a href="#">About Us</a>
+        		<a href="#">Meet the team</a>
+        		<a href="#">Contact Us</a>
+        	</nav>
+        </header>
+        <section>
+        	<h1>Our Work</h1>
+        	<p>Some description of our work comes here.</p>
+        	<!-- image grid comes here -->
+        </section>
+        <section>
+        	<h1>About Us</h1>
+        	<p>Some description about us comes here.</p>
+        	<!-- maybe what or how we work -->
+        </section>
+        <section>
+        	<h1>Meet the Team</h1>
+        	<p>Some description about the team comes here.</p>
+        	<!-- pictures and names here -->
+        </section>
+        <section>
+        	<h1>Contact Us</h1>
+        	<p>Send us some love. No reason needed, just do it!</p>
+        	<!-- contact form -->
+        </section>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/jquery-1.10.2.min.js"><\/script>')</script>
